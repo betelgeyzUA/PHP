6e8224d5e0e0df9e0d52bb890ce4e1075173dd27
Betelgeyz <betelgeyzua@gmal.com>
2017-02-14 16:29:23 +0200

    Начало

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head> </head>
+<body>
+<?php
+    if(!($f = fopen("file.txt", "r"))) {
+        die ('Error open file');
+    }
+
+    $s = fread($f, 5);
+
+    echo $s.'<br>';
+
+    $s = fread($f, 6);
+
+    echo $s.'<br>';
+
+
+    $r =fclose($f);
+
+    if ($f == true){
+        echo '<br>File close';
+    } else {
+        die ('Close file error');
+    }
+
+?>
+</body>
+<html>
